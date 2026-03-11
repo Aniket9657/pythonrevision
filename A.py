3a491357f4c490aa71f58bb0b17c3181d1a1a97c
Aniket9657 <124755066+Aniket9657@users.noreply.github.com>
2026-03-11 13:05:24 +0530

    practice

---
+++ A.py
@@ -0,0 +1,19 @@
+#print("Aniket")
+#x =int(input("enter the number:"))
+#print(x)
+
+#if x>0:
+#  print(str(x)+" is a positive number ")
+#elif x<0:
+  # print(str(x)+"is a negative number ") 
+
+#else :
+ #  print(str(x)+"is zero ")
+
+
+
+def greet(name):
+    return("hello"+name)
+print(greet(" Aniket"))
+
+
