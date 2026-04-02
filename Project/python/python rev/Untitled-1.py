729f7ce3d1508aca27d1166d2ae733caf41fe75b
Aniket9657 <124755066+Aniket9657@users.noreply.github.com>
2026-04-02 18:59:28 +0530

    basic

---
+++ Project/python/python rev/Untitled-1.py
@@ -0,0 +1,34 @@
+"""n=5
+while n>0:
+    print("  "*(5-n), end=" ")
+    print(n* " *  ")
+
+    n-=1"""
+
+"""n=0
+while n<=5:
+    print("  "*(5-n), end=" ")
+    print(n* "  *  ")
+    n+=1    """
+    
+
+
+
+
+L1 = input("Enter the List of Numbers: ")
+print(L1)
+L1 = L1.split(",")
+
+print(L1)
+L1.sort()
+print(L1)
+
+
+
+
+    
+    
+         
+                
+            
+        
